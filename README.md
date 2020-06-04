6a00a457ca40e5ab0c3e076d2fc4f9b5e8702d03
everthis <wwwaap@gmail.com>
2020-06-04 15:32:23 +0800

    Complete visualizer.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # BST-visualizer
+
+```js
+class Dnode {
+  constructor(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+}
+
+const root = new Dnode(32)
+const l1left = new Dnode(19)
+const l1right = new Dnode(14)
+const l2right = new Dnode(9)
+const l2_1 = new Dnode(8)
+const l2_2 = new Dnode(5)
+const l2_3 = new Dnode(33)
+const l3_1 = new Dnode(1)
+const l4_1 = new Dnode(99)
+root.left = l1left
+root.right = l1right
+l1right.right = l2right
+l1right.left = l2_3
+l1left.left = l2_1
+l1left.right = l2_2
+l2_1.left = l3_1
+l3_1.left = l4_1
+
+const cvs = document.getElementById("cvs")
+const tree = new BinaryTree({ canvas: cvs, root })
+
+tree.plot()
+```
+
+
+![example](https://github.com/everthis/BST-visualizer/blob/master/example.png "example")
+
--- index.js
+++ index.js
@@ -1,58 +1,88 @@
 class BinaryTree {
-  constructor({ canvas, data }) {
+  constructor({ canvas, root }) {
     const dpr = window.devicePixelRatio
     const di = canvas.getBoundingClientRect()
     canvas.setAttribute("width", dpr * di.width)
     canvas.setAttribute("height", dpr * di.height)
+    this.di = di
     this.dpr = dpr
+    this.r = 20
     this.node = canvas
-    this.data = data
+    this.data = root
+    this.padding = [20, 20, 20, 20]
     this.ctx = canvas.getContext("2d")
     this.ctx.scale(dpr, dpr)
     this.line = new Line()
     this.root = null
+    this.h = this.calcHeight()
+    this.m = new Map()
   }
-
-  add(val) {
-    if (this.root) {
-      this.recursiveAddNode(this.root, null, null, val)
-    } else {
-      this.root = this.addAndDisplayNode(300, 30, 20, this.ctx, val)
+  calcHeight() {
+    const height = node => {
+      if (node === null) return 0
+      return Math.max(height(node.left), height(node.right)) + 1
     }
+    return height(this.data)
   }
-
-  recursiveAddNode(node, prevNode, coordinateCallback, data) {
-    if (!node) {
-      const xy = coordinateCallback && coordinateCallback()
-      const newNode = this.addAndDisplayNode(xy.cx, xy.cy, 20, this.ctx, data)
-      const prevX = prevNode.getX()
-      const prevY = prevNode.getY()
-      this.line.draw(prevX, prevY, xy.cx, xy.cy, prevNode.getRadius(), this.ctx)
-      const avgX = (prevX + xy.cx) / 2
-      const avgY = (prevY + xy.cy) / 2
-      const dir = xy.cx - prevX > 0 ? "right" : "left"
-      this.ctx.fillText(dir === "right" ? 1 : 0, avgX - 3, avgY - 3)
-      return newNode
-    } else {
-      if (data <= node.getData()) {
-        node.left = this.recursiveAddNode(
-          node.left,
-          node,
-          node.leftCoordinate.bind(node),
-          data
-        )
-      } else {
-        node.right = this.recursiveAddNode(
-          node.right,
-          node,
-          node.rightCoordinate.bind(node),
-          data
-        )
+  plot() {
+    const q = [this.data]
+    let h = this.h
+    while (q.length) {
+      const size = q.length
+      for (let i = 0; i < size; i++) {
+        const cur = q.shift()
+        this.addNode(cur, h)
+        if (cur.left) q.push(cur.left)
+        if (cur.right) q.push(cur.right)
       }
-      return node
+      h--
     }
   }
-
+  addNode(node, level) {
+    if (this.root) {
+      this.recursiveAddNodeV2(node, this.data, level)
+    } else {
+      this.root = this.addAndDisplayNode(
+        this.di.width / 2,
+        this.r + this.padding[0],
+        this.r,
+        this.ctx,
+        node.val
+      )
+      this.m.set(node, this.root)
+    }
+  }
+  recursiveAddNodeV2(node, parent, level) {
+    if (node === null || parent === null) return
+    if (parent.left === node) {
+      this.plotNode(node, parent, "left", level)
+    } else if (parent.right === node) {
+      this.plotNode(node, parent, "right", level)
+    } else {
+      this.recursiveAddNodeV2(node, parent.left, level)
+      this.recursiveAddNodeV2(node, parent.right, level)
+    }
+  }
+  plotNode(node, parent, direction, level) {
+    const p = this.m.get(parent)
+    const deltaX =
+      (this.di.width - this.padding[1] - this.padding[3]) /
+      (2 * 2 ** (this.h - level))
+    let xy
+    if (direction === "right") {
+      xy = p.rightCoordinate(deltaX, this.h - level)
+    } else {
+      xy = p.leftCoordinate(deltaX, this.h - level)
+    }
+    const newNode = this.addAndDisplayNode(xy.cx, xy.cy, 20, this.ctx, node.val)
+    const [prevX, prevY] = [p.getX(), p.getY()]
+    this.line.draw(prevX, prevY, xy.cx, xy.cy, p.getRadius(), this.ctx)
+    const [avgX, avgY] = [(prevX + xy.cx) / 2, (prevY + xy.cy) / 2]
+    const text = xy.cx - prevX > 0 ? 1 : 0
+    const halfW = this.ctx.measureText(text).width
+    this.ctx.fillText(text, avgX - halfW, avgY - 2)
+    this.m.set(node, newNode)
+  }
   addAndDisplayNode(x, y, r, ctx, data) {
     const node = new Node({ val: data, left: null, right: null, x, y, r, ctx })
     node.draw()
@@ -61,7 +91,7 @@ class BinaryTree {
 }
 
 class Node {
-  constructor({ val, left, right, x, y, r, ctx }) {
+  constructor({ val, left, right, x, y, r, l, ctx }) {
     this.val = val
     this.left = left
     this.right = right
@@ -84,10 +114,6 @@ class Node {
     this.ctx.closePath()
     this.ctx.fillText(this.val, this.x - xb, this.y + yb)
   }
-
-  getData() {
-    return this.val
-  }
   getX() {
     return this.x
   }
@@ -97,11 +123,11 @@ class Node {
   getRadius() {
     return this.r
   }
-  leftCoordinate() {
-    return { cx: this.x - 3 * this.r, cy: this.y + 3 * this.r }
+  leftCoordinate(m, n) {
+    return { cx: this.x - m, cy: this.y + 3 * this.r }
   }
-  rightCoordinate() {
-    return { cx: this.x + 3 * this.r, cy: this.y + 3 * this.r }
+  rightCoordinate(m, n) {
+    return { cx: this.x + m, cy: this.y + 3 * this.r }
   }
 }
 
